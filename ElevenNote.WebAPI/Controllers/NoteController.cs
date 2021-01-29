12376d8a0bc2a899ec9abad38e0bc1122f18ff11
unknown <lindseyjrobertson@gmail.com>
2021-01-28 22:23:07 -0500

    added NoteController

---
+++ ElevenNote.WebAPI/Controllers/NoteController.cs
@@ -0,0 +1,44 @@
+﻿using ElevenNote.Models;
+using ElevenNote.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ElevenNote.WebAPI.Controllers
+{
+    [Authorize]
+    public class NoteController : ApiController
+    {
+        private NoteService CreateNoteSerivce()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var noteService = new NoteService(userId);
+            return noteService;
+        }
+
+        public IHttpActionResult Get()
+        {
+            NoteService noteService = CreateNoteSerivce();
+            var notes = noteService.GetNotes();
+            return Ok(notes);
+        }
+
+        public IHttpActionResult Post(NoteCreate note)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateNoteSerivce();
+
+            if (!service.CreateNote(note))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+    }
+}
